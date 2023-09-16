d18834d53f4b300fcf31d23a193eb20b86266f38
MiFit1 <MiFit1@yandex.ru>
2023-09-16 08:17:58 +0700

    Код изменён в соответствии с Google style

--- Lab1OPPO/main.cpp
+++ Lab1OPPO/main.cpp
@@ -4,119 +4,108 @@
 #include <vector>
 using namespace std;
 
-class date {
-private:
-	int year;
-	int month;
-	int day;
-
-	bool setDate(int year, int month, int day);
+class Date {
 public:
-	date() : year(0), month(0), day(0) {}
-	~date() {};
-	bool read(std::istream& ist);
-	void print(std::ostream& ost);
+	Date() : year_(0), month_(0), day_(0) {}
+	~Date() {};
+	bool ReadDate(std::istream& ist);
+	void PrintDate(std::ostream& ost);
+private:
+	int year_;
+	int month_;
+	int day_;
+
+	bool SetDate(int year_, int month_, int day_);
 };
-void date::print(std::ostream& ost)
-{
-	ost << year << ".";
-	ost << month << ".";
-	ost << day << " ";
+void Date::PrintDate(std::ostream& ost){
+	ost << year_ << ".";
+	ost << month_ << ".";
+	ost << day_ << " ";
 }
 
-bool date::read(std::istream& ist)
-{
-	string buff;
-	ist >> buff;
-	year = stoi(buff.substr(0, 4));
-	month = stoi(buff.substr(5, 7));
-	day = stoi(buff.substr(8, 9));
+bool Date::ReadDate(std::istream& ist){
+	string buffer;
+	ist >> buffer;
+	year_ = stoi(buffer.substr(0, 4));
+	month_ = stoi(buffer.substr(5, 7));
+	day_ = stoi(buffer.substr(8, 9));
 	return true;
 }
 
-class income
+class Income
 {
 private:
-	date dateReceip;
-	string source;
-	int sum;
+	Date date_receip_;
+	string source_;
+	int sum_;
 	
-	bool readSource(std::istream& ist);
-	bool readSum(std::istream& ist);
+	bool ReadSource(std::istream& ist);
+	bool ReadSum(std::istream& ist);
 public:
-	income() {};
-	~income() {};
-	bool read(std::istream& ist);
-	void print(std::ostream& ost);
+	Income() {};
+	~Income() {};
+	bool ReadIncome(std::istream& ist);
+	void PrintDate(std::ostream& ost);
 };
-void income::print(std::ostream& ost)
-{
-	dateReceip.print(ost);
-	ost << source << " ";
-	ost << sum << endl;
+void Income::PrintDate(std::ostream& ost){
+	date_receip_.PrintDate(ost);
+	ost << source_ << " ";
+	ost << sum_ << endl;
 }
-bool income::readSum(std::istream& ist)
-{
-	string buff;
-	ist >> buff;
-	sum = stoi(buff);
+bool Income::ReadSum(std::istream& ist){
+	string buffer;
+	ist >> buffer;
+	sum_ = stoi(buffer);
 	return true;
 }
 
-bool income::readSource(std::istream& ist)
-{
-	string buff;
+bool Income::ReadSource(std::istream& ist){
+	string buffer;
 	while (true)
 	{
-		ist >> buff;
-		source.append(buff + " ");
-		if (buff.back() == '"')
-		{
+		ist >> buffer;
+		source_.append(buffer + " ");
+		if (buffer.back() == '"'){
 			break;
 		}
 	}
-	source.erase(source.end() - 2, source.end()--);
-	source.erase(source.begin());
+	source_.erase(source_.end() - 2, source_.end()--);
+	source_.erase(source_.begin());
 
 	return true;
 }
-bool income::read(std::istream& ist)
-{
-	dateReceip.read(ist);
-	readSource(ist);
-	readSum(ist);
+bool Income::ReadIncome(std::istream& ist){
+	date_receip_.ReadDate(ist);
+	ReadSource(ist);
+	ReadSum(ist);
 
 	return true;
 }
 
-void printDatabase(std::vector<income>& dataBase)
+void PrintDatabase(std::vector<Income>& dataBase)
 {
-	vector<income>::iterator it;
-	for (it = dataBase.begin(); it != dataBase.end(); it++)
-	{
-		income inc = *it;
-		inc.print(cout);
+	vector<Income>::iterator it;
+	for (it = dataBase.begin(); it != dataBase.end(); it++){
+		Income inc = *it;
+		inc.PrintDate(cout);
 	}
 }
-bool readDataBase(std::vector<income>& dataBase)
+bool ReadDatabase(std::vector<Income>& dataBase)
 {
 	ifstream in("in.txt");
-	if (!in.is_open())
-	{
+	if (!in.is_open()){
 		return false;
 	}
-	while (!in.eof())
-	{
-		income inc;
-		inc.read(in);
+	while (!in.eof()){
+		Income inc;
+		inc.ReadIncome(in);
 		dataBase.push_back(inc);
 	}
 	return true;
 }
-int main()
-{
-	vector<income> dataBase;
-	readDataBase(dataBase);
-	printDatabase(dataBase);
+int main(){
+	vector<Income> dataBase;
+	ReadDatabase(dataBase);
+	PrintDatabase(dataBase);
 	return 0;
 }
